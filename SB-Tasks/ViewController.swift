f8d50db291042bfe6dd153fa0f1230e4f4b898fe
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-18 10:38:40 +0700

    Create the project

---
+++ SB-Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB-Tasks
+//
+//  Created by Danh Tu on 18/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
