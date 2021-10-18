b841a8ec4b5631678dc78e0a7dbc123e8f694ae6
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-18 14:42:27 +0700

    TableView Outlet

---
+++ SB-Tasks/EntryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EntryViewController.swift
+//  SB-Tasks
+//
+//  Created by Danh Tu on 18/10/2021.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ SB-Tasks/TaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TaskViewController.swift
+//  SB-Tasks
+//
+//  Created by Danh Tu on 18/10/2021.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
